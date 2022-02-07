d6a1d1c0e9815fd94a7944c5e7331860963a8b38
JuanSilva27 <juan.silva.0270@gmail.com>
2022-02-07 17:32:32 -0300

    se crea proyecto en react

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+function Banner() {
+    return (
+        <section class="banner">
+            <h1 class="titulo">Tenemos lo mejor para ti...</h1>
+        </section>
+    )
+}
+
+export default Banner
